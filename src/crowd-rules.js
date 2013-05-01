5d72f7662c5d9ebcbe05af4cd15a000d77c9a0a8
Dmitry Bragin <dbragin@aboutecho.com>
2013-05-01 10:02:23 +0400

    Added submit form

--- src/crowd-rules.js
+++ src/crowd-rules.js
@@ -7,10 +7,34 @@ var CrowdRules = Echo.App.manifest("Echo.Apps.CrowdRules");
 
 if (Echo.App.isDefined("Echo.Apps.CrowdRules")) return;
 
-CrowdRules.templates.main =
-	'<div id="{class:container}"></div>';
+CrowdRules.config = {
+	"submit": {}
+};
 
-CrowdRules.renderers.container = function(element) {
+CrowdRules.dependencies = [{
+	"loaded": function() {
+		return Echo.Control.isDefined("Echo.StreamServer.Controls.Stream") &&
+			Echo.Control.isDefined("Echo.StreamServer.Controls.Subsmit");
+	},
+	"url": "{config:cdnBaseURL.sdk}/streamserver.pack.js"
+}];
+
+CrowdRules.templates.main =
+	'<div class="{class:container}">' +
+		'<div class="{class:submit}"></div>' +
+		'<div class="{class:tabs}"></div>' +
+	'</div>';
+
+CrowdRules.renderers.submit = function(element) {
+	new Echo.StreamServer.Controls.Submit($.extend({
+		"target": element,
+		"appkey": this.config.get("appkey"),
+		"targetURL": "http://example.com/crowdrules"
+	}, this.config.get("submit")));
+	return element;
+};
+
+CrowdRules.renderers.tabs = function(element) {
 	return element;
 };
 
